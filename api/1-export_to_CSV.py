3325b0d8ee4062b2b2d20afbdb94a1681df3a672
afr117 <54492982+afr117@users.noreply.github.com>
2024-07-18 21:43:28 -0400

    Update 1-export_to_CSV.py
    pep8 t1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,8 +7,10 @@ import csv
 import requests
 import sys
 
+
 def fetch_employee_todo_csv(employee_id):
-    """Fetch TODO list for a given employee ID from JSONPlaceholder API and export to CSV."""
+    """Fetch TODO list for a given employee ID
+    from JSONPlaceholder API and export to CSV."""
     base_url = 'https://jsonplaceholder.typicode.com'
     user_url = f'{base_url}/users/{employee_id}'
     todos_url = f'{base_url}/todos?userId={employee_id}'
@@ -27,7 +29,8 @@ def fetch_employee_todo_csv(employee_id):
         username = user_data.get('name')
         csv_filename = f"{employee_id}.csv"
 
-        with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
+        with open(csv_filename, mode='w',
+                  newline='', encoding='utf-8') as file:
             writer = csv.writer(file, quoting=csv.QUOTE_ALL)
             for task in todos_data:
                 writer.writerow([
@@ -41,6 +44,7 @@ def fetch_employee_todo_csv(employee_id):
 
     except requests.exceptions.RequestException as e:
         print(f"Error fetching data: {e}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
